7e065a30e15ba3b9a36fdcb2b7c8dd24dba6678a
robester0403 <robkso@gmail.com>
2021-11-27 09:22:50 -0500

    fixed some issues and tested routes in react-router-dom 6

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import './App.css';
+import FrontPage from "./pages/FrontPage/FrontPage"
+// import UserHomePage from "./pages/UserHomePage/UserHomePage"
 
 document.title = "Crumbs";
 
 const App = () => {
   return (
     <>
-      This is the front Landing/login page
+      <Routes>
+        {/* Landing page route */}
+        <Route exact path="/" component={FrontPage} />
+
+        {/* Stage 1: route to default map & list of reviews from foodiemamaStage 2: route to a user's specific account */}
+        {/* <Route path="/userview/" component={UserHomePage} /> */}
+        
+      </Routes>
     </>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,26 +1,20 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
-const userRoutes = require("./routes/users");
-
+const app = express();
+// const userRoutes = require("./routes/users");
 // Middleware
 app.use(cors());
 app.use(express.json());
+
 app.use(express.static("public"));
 
 // Config
 require("dotenv").config();
 const port = process.env.PORT || 8080;
 
-app.use("/api/users", userRoutes)
+// app.use("/api/users", userRoutes)
 
-// get driver connection
-// const dbo = require("./db/conn");
 app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-    else (console.log("db connected"))
-  });
-  console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on PORT ${port}`);
 });
+
