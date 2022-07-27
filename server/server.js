d55a191e2c7b096e28b0ce26a27b33c9ef8df1bf
robester0403 <robkso@gmail.com>
2022-07-27 02:14:52 -0400

    Setting up server for heroku and created the static build for the front end.

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const usersRoutes = require("./routes/users-routes");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
@@ -30,14 +31,12 @@ app.use("/api/users", usersRoutes);
 // });
 
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.ATLAS_URI)
-  .then(() => {
-    app.listen(5000);
-    console.log(`Connected to Port ${port}`);
-  })
+  .connect(process.env.ATLAS_URI) // may need a fallback DB?
   .catch((err) => {
     console.log(err);
   });
+
+app.listen(PORT, console.log(`Connected to Port ${PORT}`));
