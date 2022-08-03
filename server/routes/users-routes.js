8775a192707fc06de92cd9a27008bb02df0b57a1
robester0403 <robkso@gmail.com>
2022-08-02 22:09:23 -0400

    better file indentation

--- server/routes/users-routes.js
+++ server/routes/users-routes.js
@@ -1,33 +1,35 @@
-const express = require('express');
-const { check } = require('express-validator');
-const checkAuth = require('../middleware/check-auth');
-const usersController = require('../controllers/users-controllers');
+const express = require("express");
+const { check } = require("express-validator");
+const checkAuth = require("../middleware/check-auth");
+const usersController = require("../controllers/users-controllers");
 
 const router = express.Router();
 
-router.get('/', usersController.getUsers);
+router.get("/", usersController.getUsers);
 
 router.post(
-  '/signup',
+  "/signup",
   [
-    check('name')
-      .not()
-      .isEmpty(),
-    check('email')
-      .normalizeEmail()
-      .isEmail(),
-    check('password').isLength({ min: 6 })
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 }),
   ],
   usersController.signup
 );
 
-router.post('/login', usersController.login);
+router.post("/login", usersController.login);
 
 router.use(checkAuth);
 
-router.post('/loggedin/:userId/searchYelp', usersController.influencerSearchYelp)
-router.get('/loggedin/:userId/profile', usersController.influencerDBGetProfile);
-router.post('/loggedin/:userId/addmarkinst', usersController.influencerDBAddMarkerInstance);
-router.get('/loggedin/:userId', usersController.influencerDBGetOwnInstances);
+router.post(
+  "/loggedin/:userId/searchYelp",
+  usersController.influencerSearchYelp
+);
+router.get("/loggedin/:userId/profile", usersController.influencerDBGetProfile);
+router.post(
+  "/loggedin/:userId/addmarkinst",
+  usersController.influencerDBAddMarkerInstance
+);
+router.get("/loggedin/:userId", usersController.influencerDBGetOwnInstances);
 
 module.exports = router;
