61edd70188c9a6aebb2f066b10056eeaa0602589
robester0403 <robkso@gmail.com>
2022-08-16 15:16:38 -0400

    new

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import './App.css';
-import FrontPage from "./pages/FrontPage/FrontPage"
-import UserHomePage from "./pages/UserHomePage/UserHomePage"
-import LogIn from "./pages/LogIn/LogIn"
-import SignUp from "./pages/SignUp/SignUp"
-import BloggerDashboard from "./protectedpages/BloggerDashboard/BloggerDashboard"
-import BloggerEditProfile from "./protectedpages/BloggerEditProfile/BloggerEditProfile";
+import "./App.css";
+import FrontPage from "./pages/FrontPage/FrontPage";
+import UserHomePage from "./pages/UserHomePage/UserHomePage";
+import LogIn from "./pages/LogIn/LogIn";
+import SignUp from "./pages/SignUp/SignUp";
+import BloggerDashboard from "./protectedpages/BloggerDashboard/BloggerDashboard";
+// import BloggerEditProfile from "./protectedpages/BloggerEditProfile/BloggerEditProfile";
 
 document.title = "Crumbs: blogger mapper experience";
 
@@ -13,16 +13,19 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<FrontPage/>} />
-        <Route path="/userhome" element={<UserHomePage/>} />
-        <Route path="/login" element={<LogIn/>} />
-        <Route path="/signup" element={<SignUp/>} />
-        <Route path="/login/blogger/:userId" element={<BloggerDashboard/>} />
-        <Route path="/login/blogger/:userId/Upload" element={<BloggerDashboard/>} />
+        <Route path="/" element={<FrontPage />} />
+        <Route path="/userhome" element={<UserHomePage />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login/blogger/:userId" element={<BloggerDashboard />} />
+        <Route
+          path="/login/blogger/:userId/Upload"
+          element={<BloggerDashboard />}
+        />
         {/* <Route path="/login/blogger/:userId/editprofile" element={<BloggerEditProfile/>} /> */}
       </Routes>
     </>
   );
-}
+};
 
 export default App;
