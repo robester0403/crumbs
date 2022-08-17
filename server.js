cfd954d37400532077d43c419923f20bfbc11f23
robester0403 <robkso@gmail.com>
2022-08-17 12:13:37 -0400

    Heroku

--- server.js
+++ server.js
@@ -37,15 +37,15 @@ app.use("/api/instancemap", instanceMapRoutes);
 app.use("/api/users", usersRoutes);
 
 // Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 // Step 2:
 app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.get("*", (_, response) => {
-    response.sendFile(path.join(__dirname, "client/build", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 const PORT = process.env.PORT || 8080;
