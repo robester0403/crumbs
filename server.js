b557dd37f5c118ee95490528b15355178520b17f
robester0403 <robkso@gmail.com>
2022-08-17 10:59:29 -0400

    Heroku

--- server.js
+++ server.js
@@ -10,14 +10,13 @@ const morgan = require("morgan");
 
 const app = express();
 
-// refactor this into a new file
 require("dotenv").config();
 
 mongoose
   .connect(process.env.ATLAS_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }) // may need a fallback DB?
+  })
   .then(console.log("Mongoose is connected"))
   .catch((err) => {
     console.log(err);
@@ -36,21 +35,10 @@ app.use(morgan("combined"));
 app.use("/api/instancemap", instanceMapRoutes);
 app.use("/api/users", usersRoutes);
 
-// app.use((error, req, res, next) => {
-//   if (req.file) {
-//     fs.unlink(req.file.path, (err) => {
-//       console.log(err);
-//     });
-//   }
-//   if (res.headerSent) {
-//     return next(error);
-//   }
-//   res.status(error.code || 500);
-//   res.json({ message: error.message || "An unknown error occurred!" });
-// });
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(_dirname, "/client/build")));
+  app.get("*", (_, response) => {
+    response.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
 }
 
 app.listen(process.env.PORT || 5000, console.log(`Connected to Port ${PORT}`));
