7f8562a4e22eef7423979a99693c68190223c973
robester0403 <robkso@gmail.com>
2022-08-18 10:12:37 -0400

    Heroku

--- server.js
+++ server.js
@@ -44,9 +44,7 @@ app.get("*", function (request, response) {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
+  app.use("*", express.static(path.join(__dirname, "client", "build")));
 }
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
