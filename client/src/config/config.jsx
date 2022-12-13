2098b0620a0a13dcae675f5e98aa0edeb1b829e6
robester0403 <robkso@gmail.com>
2022-12-13 13:54:49 -0500

    Changed heroku config

--- client/src/config/config.jsx
+++ client/src/config/config.jsx
@@ -1,4 +1,4 @@
-export const API_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://crumbsmapinfluencertrails.herokuapp.com"
-    : "https://localhost:8080";
+export const API_URL = "https://crumbsmapinfluencertrails.herokuapp.com";
+// process.env.NODE_ENV === "production"
+//   ? "https://crumbsmapinfluencertrails.herokuapp.com"
+//   : "https://localhost:8080";
